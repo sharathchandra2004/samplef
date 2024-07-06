15189c3123e41cef6fda837f5214b77ff203011f
Flutter <crsharath242004@gmail.com>
2024-07-06 16:22:57 +0530

    mongo

---
+++ lib/pages/constant.dart
@@ -0,0 +1,3 @@
+const MONGO_CONN_URL="mongodb+srv://varunks775:g*ymM2-yHf4*TWw@faculty.g5viekc.mongodb.net/teachers_timetable?retryWrites=true&w=majority&appName=faculty";
+const USER_COLLECTION = "aswath";
+
---
+++ lib/pages/mongodb.dart
@@ -0,0 +1,22 @@
+import 'dart:developer';
+// ignore: depend_on_referenced_packages
+import 'package:mongo_dart/mongo_dart.dart';
+
+import 'package:samplef/pages/constant.dart';
+
+class MongoDatabase {
+  // ignore: prefer_typing_uninitialized_variables
+    static var db , userCollection;
+    static connect() async{
+      try{
+         db =await Db.create(MONGO_CONN_URL);
+      await db.open();
+      inspect(db);
+      userCollection = db.collection(USER_COLLECTION);
+      print("Database connected successfully");
+      }
+      catch(e){
+        print("error connecting to database:$e");
+      }
+    }
+ }
